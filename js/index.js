fd2e2aedd5d2fc210b88aec6d9e8d5d1ad82b791
stojchevskiboris <stojcevskib.02@gmail.com>
2022-09-08 16:05:03 +0200

    Add EmailJS on contact.html

--- js/index.js
+++ js/index.js
@@ -3,4 +3,31 @@ $(document).ready(function () {
     var date = new Date(time);
     $("#timeNow").html(date.getFullYear());
 
+    $("#hoverlinkedin").on('mouseover', function() {
+        $("#hoverlinkedin").attr('src','../content/linkedin_hover.png')
+    });
+    $("#hoverlinkedin").on('mouseout', function() {
+        $("#hoverlinkedin").attr('src','../content/linkedin.png')
+    });
+
+    $("#hovergithub").on('mouseover', function() {
+        $("#hovergithub").attr('src','../content/github_hover.png')
+    });
+    $("#hovergithub").on('mouseout', function() {
+        $("#hovergithub").attr('src','../content/github.png')
+    });
+
+    $("#hoverfacebook").on('mouseover', function() {
+        $("#hoverfacebook").attr('src','../content/facebook_hover.png')
+    });
+    $("#hoverfacebook").on('mouseout', function() {
+        $("#hoverfacebook").attr('src','../content/facebook.png')
+    });
+
+    $("#hoverinstagram").on('mouseover', function() {
+        $("#hoverinstagram").attr('src','../content/instagram_hover.png')
+    });
+    $("#hoverinstagram").on('mouseout', function() {
+        $("#hoverinstagram").attr('src','../content/instagram.png')
+    });
 })
