17d21c0513d850d458ca310ccc737b6ade7e1371
stojchevskiboris <stojcevskib.02@gmail.com>
2022-09-12 16:56:21 +0200

    Update index and contact pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,52 @@
+var i = 0;
+var j=0;
+var txt = '>\n \n \n \n Hi, \n \n \n \n \n \n my name is' +
+    '    Boris Stojchevski. \n' +
+    '    >\n \n \n \n I`m a software developer. \n \n' +
+    ' \n \n' +
+    '    >\n \n \n \n Currently I`m a student at Faculty of Computer Science and Engineering - Skopje.' +
+    '    >\n \n \n \n I`m open to work and for intership :).' +
+    '    > “Learning to code is learning to create and innovate”';
+var speed = 60;
+function start(){
+    typeWriter();
+    typeWriter2();
+}
+function typeWriter() {
+    if (i < txt.length) {
+        if(i<txt.length){
+            if (txt.charAt(i-1)=='.'){
+                document.getElementById("main1").innerHTML += "<br>";
+            }
+        }
+        document.getElementById("main1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+// var txt2 = '“The best programs are written so that computing machines ' +
+//     'can perform them quickly and so that human beings can understand ' +
+//     'them clearly. A programmer is ideally an essayist who works with ' +
+//     'traditional aesthetic and literary forms as well as mathematical ' +
+//     'concepts, to communicate the way that an algorithm works and to ' +
+//     'convince a reader that the results will be correct.”\n' +
+//     '― Donald E. Knuth, Selected Papers on Computer Science'
+// function typeWriter2() {
+//     if (j < txt.length) {
+//         if(j<txt.length){
+//             if (txt.charAt(j-1)=='.'){
+//                 document.getElementById("main2").innerHTML += "<br>";
+//             }
+//         }
+//         document.getElementById("main2").innerHTML += txt2.charAt(j);
+//         j++;
+//         setTimeout(typeWriter2, speed);
+//     }
+// }
+
+
+
 $(document).ready(function () {
     var time = new Date().getTime();
     var date = new Date(time);
@@ -30,4 +79,7 @@ $(document).ready(function () {
     $("#hoverinstagram").on('mouseout', function() {
         $("#hoverinstagram").attr('src','../content/instagram.png')
     });
+
+
+
 })
