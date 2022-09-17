e48aee78062ed4be62b14ba03784e1234cce80a1
stojchevskiboris <stojcevskib.02@gmail.com>
2022-09-17 10:08:14 +0200

    Add certificates div

--- js/about.js
+++ js/about.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
         autoOpen: false,
         modal: false,
         buttons: {
+            Download: function (){
+                window.open("../content/CV.pdf")
+            },
             Close: function () {
                 $("#modal_div").dialog("close")
             }
@@ -11,10 +14,10 @@ $(document).ready(function () {
     })
     $("#cvButton").click(function () {
         $("#modal_div").dialog("open")
-        $(".ui-dialog").css("width", "800")
-        $("#cvPhoto").css("width","740")
+        $(".ui-dialog").css("width", "80vw")
+        $("#cvPhoto").css("width","75vw")
         $("#cvPhoto").css("height","auto")
-        $(".ui-dialog").css("height", "400")
+        $(".ui-dialog").css("height", "61vh")
         $(".ui-dialog").css("overflow-y", "scroll")
         $(".ui-dialog").css("inset", "100px 0px 0px 50px")
         // window.scrollTo(0,0);
@@ -59,4 +62,131 @@ $(document).ready(function () {
     $("#hoverhome").on('mouseout', function() {
         $("#hoverhome").attr('src','../content/b.png')
     });
+
+
+    // Animation
+
+    (function ($) {
+        $.fn.countTo = function (options) {
+            options = options || {};
+
+            return $(this).each(function () {
+                // set options for current element
+                var settings = $.extend({}, $.fn.countTo.defaults, {
+                    from:            $(this).data('from'),
+                    to:              $(this).data('to'),
+                    speed:           $(this).data('speed'),
+                    refreshInterval: $(this).data('refresh-interval'),
+                    decimals:        $(this).data('decimals')
+                }, options);
+
+                // how many times to update the value, and how much to increment the value on each update
+                var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                    increment = (settings.to - settings.from) / loops;
+
+                // references & variables that will change with each update
+                var self = this,
+                    $self = $(this),
+                    loopCount = 0,
+                    value = settings.from,
+                    data = $self.data('countTo') || {};
+
+                $self.data('countTo', data);
+
+                // if an existing interval can be found, clear it first
+                if (data.interval) {
+                    clearInterval(data.interval);
+                }
+                data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+                // initialize the element with the starting value
+                render(value);
+
+                function updateTimer() {
+                    value += increment;
+                    loopCount++;
+
+                    render(value);
+
+                    if (typeof(settings.onUpdate) == 'function') {
+                        settings.onUpdate.call(self, value);
+                    }
+
+                    if (loopCount >= loops) {
+                        // remove the interval
+                        $self.removeData('countTo');
+                        clearInterval(data.interval);
+                        value = settings.to;
+
+                        if (typeof(settings.onComplete) == 'function') {
+                            settings.onComplete.call(self, value);
+                        }
+                    }
+                }
+
+                function render(value) {
+                    var formattedValue = settings.formatter.call(self, value, settings);
+                    $self.html(formattedValue);
+                }
+            });
+        };
+
+        $.fn.countTo.defaults = {
+            from: 0,               // the number the element should start at
+            to: 0,                 // the number the element should end at
+            speed: 1000,           // how long it should take to count between the target numbers
+            refreshInterval: 100,  // how often the element should be updated
+            decimals: 0,           // the number of decimal places to show
+            formatter: formatter,  // handler for formatting the value before rendering
+            onUpdate: null,        // callback method for every time the element is updated
+            onComplete: null       // callback method for when the element finishes updating
+        };
+
+        function formatter(value, settings) {
+            return value.toFixed(settings.decimals);
+        }
+    }(jQuery));
+
+    jQuery(function ($) {
+        // custom formatting example
+        $('.count-number').data('countToOptions', {
+            formatter: function (value, options) {
+                return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+            }
+        });
+
+        // start all the timers
+        $('.timer').each(count);
+
+        function count(options) {
+            var $this = $(this);
+            options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+            $this.countTo(options);
+        }
+    });
+
+    // Animation
+
+    $( "#certButton" ).click(function() {
+        if ( $( "#certificates" ).first().is( ":hidden" ) ) {
+            $( "#certificates" ).slideDown( "slow" );
+        } else {
+            $( "#certificates" ).slideUp( "slow" );
+        }
+    });
+    $("#linkedh2").click(function (){
+        if ( $( "#linCerts" ).first().is( ":hidden" ) ) {
+            $( "#linCerts" ).slideDown( "slow" );
+            $( "#linkedh2 img").attr('src', '../content/arrow_down_grey.png');
+        } else {
+            $( "#linCerts" ).slideUp( "slow" );
+            $( "#linkedh2 img").attr('src', '../content/arrow_right_grey.png');
+        }
+    });
+
 })
+
+function start2(){
+    // alert("test");
+    $("html, body").animate({ scrollTop: document.body.offsetHeight }, 3000);
+}
