ee66e6c1d2f292514860a8220830d615f3723716
stojchevskiboris <stojcevskib.02@gmail.com>
2022-09-28 14:38:44 +0200

    Update projects

--- js/projects.js
+++ js/projects.js
@@ -37,8 +37,88 @@ $(document).ready(function () {
     $("#hoverhome").on('mouseout', function() {
         $("#hoverhome").attr('src','../content/b.png')
     });
+    var pNum = 1;
+    $(".top").click(function (){
+        $(this).parent('div').fadeOut();
+        $("#projectLogoDesigns").fadeOut();
+        $("#projectPortWeb").fadeOut();
+        $("#projectAvtokamp").fadeOut();
+        setTimeout(function(){
+            $("#allCards").fadeIn();
+        }, 390);
 
-    $("#avtokamp").click(function (){
-        alert("test");
     })
+    $("#avtokamp").click(function (){
+        $("#allCards").fadeOut();
+        setTimeout(function(){
+            $("#projectAvtokamp").fadeIn();
+            }, 390);
+        pNum = 1;
+    })
+    $("#logos").click(function (){
+        $("#allCards").fadeOut();
+        setTimeout(function(){
+            $("#projectLogoDesigns").fadeIn();
+        }, 390);
+        pNum = 2;
+    })
+    $("#portweb").click(function (){
+        $("#allCards").fadeOut();
+        setTimeout(function(){
+            $("#projectPortWeb").fadeIn();
+        }, 390);
+        pNum = 3;
+    })
+
+    $(".prev").click(function (){
+        if (pNum==1)
+            pNum=3;
+        else {
+            pNum--;
+        }
+        checkpNum();
+    })
+    $(".next").click(function (){
+        if (pNum==3)
+            pNum=1;
+        else {
+            pNum++;
+        }
+        checkpNum();
+    })
+
+
+    function checkpNum(){
+        console.log(pNum)
+        if (pNum==1){
+            //close remaining views
+            $("#projectLogoDesigns").fadeOut();
+            $("#projectPortWeb").fadeOut();
+            //open actual view
+            setTimeout(function(){
+                $("#projectAvtokamp").fadeIn();
+            }, 390);
+        }
+
+        if (pNum==2){
+            //close remaining views
+            $("#projectAvtokamp").fadeOut();
+            $("#projectPortWeb").fadeOut();
+            //open actual view
+            setTimeout(function(){
+                $("#projectLogoDesigns").fadeIn();
+            }, 390);
+        }
+
+        if (pNum==3){
+            //close remaining views
+            $("#projectAvtokamp").fadeOut();
+            $("#projectLogoDesigns").fadeOut();
+            //open actual view
+            setTimeout(function(){
+                $("#projectPortWeb").fadeIn();
+            }, 390);
+        }
+    }
+
 })
